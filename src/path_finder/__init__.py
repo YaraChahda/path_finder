fd65ae040f8a9f8a4963aae1bcd85e8bbe276c7d
InesOuchen <ines.ouchen@epfl.ch>
2026-05-01 00:25:53 +0200

    update

---
+++ src/path_finder/__init__.py
@@ -0,0 +1,5 @@
+"""This package aims to find the best retro synthesis pathways for some drugs, based on criteria selected by the user.."""
+
+from __future__ import annotations
+
+__version__ = "0.0.1"
