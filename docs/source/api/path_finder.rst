da22c1f1455873a75c9aaa94bf146778d553f1d8
InesOuchen <ines.ouchen@epfl.ch>
2026-05-19 12:13:43 +0200

    tox modif

--- docs/source/api/path_finder.rst
+++ docs/source/api/path_finder.rst
@@ -4,50 +4,66 @@ path\_finder package
 Submodules
 ----------
 
-path\_finder.app\_path\_finder module
--------------------------------------
+path\_finder.app module
+-----------------------
 
-.. automodule:: path_finder.app_path_finder
+.. automodule:: path_finder.app
    :members:
-   :show-inheritance:
    :undoc-members:
+   :show-inheritance:
+
+path\_finder.app\_utensils module
+---------------------------------
+
+.. automodule:: path_finder.app_utensils
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+path\_finder.launch module
+--------------------------
+
+.. automodule:: path_finder.launch
+   :members:
+   :undoc-members:
+   :show-inheritance:
 
 path\_finder.localization module
 --------------------------------
 
 .. automodule:: path_finder.localization
    :members:
-   :show-inheritance:
    :undoc-members:
+   :show-inheritance:
 
 path\_finder.molecule\_rendering module
 ---------------------------------------
 
 .. automodule:: path_finder.molecule_rendering
    :members:
-   :show-inheritance:
    :undoc-members:
+   :show-inheritance:
 
 path\_finder.report\_builder module
 -----------------------------------
 
 .. automodule:: path_finder.report_builder
    :members:
-   :show-inheritance:
    :undoc-members:
+   :show-inheritance:
 
 path\_finder.route\_engine module
 ---------------------------------
 
 .. automodule:: path_finder.route_engine
    :members:
-   :show-inheritance:
    :undoc-members:
+   :show-inheritance:
 
 Module contents
 ---------------
 
 .. automodule:: path_finder
    :members:
-   :show-inheritance:
    :undoc-members:
+   :show-inheritance:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,42 +1,28 @@
 """Sphinx configuration."""
-
-# This file is execfile()d with the current directory set to its containing dir.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-#
-# All configuration values have a default; values that are commented out
-# serve to show the default.
-
 import os
 import shutil
 import sys
 from importlib.metadata import metadata
 
-# -- Path setup 
+# -- Path setup ---------------------------------------------------------------
 
 __location__ = os.path.dirname(__file__)
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.join(__location__, "../../src"))
 
-# -- Run sphinx-apidoc 
+# -- Run sphinx-apidoc --------------------------------------------------------
 # This hack is necessary since RTD does not issue `sphinx-apidoc` before running
-# `sphinx-build -b html . _build/html`. See Issue:
+# `sphinx-build -b html . _build/html`. See:
 # https://github.com/readthedocs/readthedocs.org/issues/1139
-# DON'T FORGET: Check the box "Install your project inside a virtualenv
-# Additionally it helps us to avoid running apidoc manually
 
-try:  # for Sphinx >= 1.7
+try:
     from sphinx.ext import apidoc
 except ImportError:
     from sphinx import apidoc
 
 output_dir = os.path.join(__location__, "api")
 module_dir = os.path.join(__location__, "../../src/path_finder")
+
 try:
     shutil.rmtree(output_dir)
 except FileNotFoundError:
@@ -44,31 +30,25 @@ except FileNotFoundError:
 
 try:
     import sphinx
-
     cmd_line = f"sphinx-apidoc --implicit-namespaces -f -o {output_dir} {module_dir}"
-
     args = cmd_line.split(" ")
     if tuple(sphinx.__version__.split(".")) >= ("1", "7"):
-        # This is a rudimentary parse_version to avoid external dependencies
         args = args[1:]
-
     apidoc.main(args)
 except Exception as e:
     print(f"Running `sphinx-apidoc` failed!\n{e}")
 
-# -- Project information
+# -- Project information ------------------------------------------------------
 
-_metadata = metadata("path_finder")
+_metadata = metadata("path-finder-retrosynthesis")
 
-project = _metadata["Name"]
-author = _metadata["Author-email"].split("<", 1)[0].strip()
+project   = "path-finder-retrosynthesis"
+author    = "Yara Chahda, Corentin Portmann, Ines Ouchen"
 copyright = f"2024, {author}"
+version   = _metadata["Version"]
+release   = ".".join(version.split(".")[:2])
 
-version = _metadata["Version"]
-release = ".".join(version.split(".")[:2])
-
-
-# -- General configuration
+# -- General configuration ----------------------------------------------------
 
 extensions = [
     "myst_parser",
@@ -78,19 +58,11 @@ extensions = [
     "sphinx.ext.viewcode",
 ]
 
-templates_path = ["_templates"]
+templates_path   = ["_templates"]
+exclude_patterns = ["Thumbs.db", ".DS_Store", ".ipynb_checkpoints"]
+suppress_warnings = ["myst.header"]
 
-exclude_patterns = [
-    "Thumbs.db",
-    ".DS_Store",
-    ".ipynb_checkpoints",
-]
+# -- Options for HTML output --------------------------------------------------
 
-suppress_warnings = [
-    'myst.header'
-]
-
-# -- Options for HTML output
-
-html_theme = "furo"
+html_theme       = "furo"
 html_static_path = ["_static"]
