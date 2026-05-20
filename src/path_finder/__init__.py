fe4c6150d33f073cbd43bcbcfaaadbc3141b1df1
InesOuchen <ines.ouchen@epfl.ch>
2026-05-20 10:40:30 +0200

    Update year in developer credits to 2026

--- src/path_finder/__init__.py
+++ src/path_finder/__init__.py
@@ -4,7 +4,7 @@ path_finder
 Find the best retrosynthesis pathways for target molecules,
 based on criteria selected by the user.
 
-Developed by Yara Chahda, Corentin Postmann and Ines Ouchen — EPFL 2025.
+Developed by Yara Chahda, Corentin Portmann and Ines Ouchen — EPFL 2026.
 
 After pip install path-finder-retrosynthesis:
     path-finder-setup    # first-time setup
